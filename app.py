47f5db9e6b00404275c92ded4f00e2e2e4d30917
zoown13 <41746120+zoown13@users.noreply.github.com>
2021-11-29 18:09:47 +0900

    Amazon DynamoDB와 연결
    1. Login시 DB에 유저가 존재하는지 확인 기능 추가
    2. Signup시  DB에 유저 등록 기능 추가

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 # -- coding: utf-8 --
 from flask import Flask, request, render_template, session, flash, redirect, url_for
 import config
+import controller as dynamodb
 
 app = Flask(__name__)
 app.secret_key = 'user_key'
+
+
+@app.route('/')
+def index():
+    dynamodb.create_table_user()
+    return 'Table Created!'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['email'] != config.USERNAME:
-            error = 'Invalid username'
-            flash('Invalid username')
-        elif request.form['password'] != config.PASSWORD:
-            error = 'Invalid password'
-            flash('Invalid password')
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('login'))
+        response = dynamodb.read_user_information(request.form['email'])
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            if 'Item' in response:
+                if request.form['email'] != response['Item']['Email']:
+                    error = 'Invalid username'
+                    flash('Invalid username')
+                elif request.form['password'] != response['Item']['Password']:
+                    error = 'Invalid password'
+                    flash('Invalid password')
+                else:
+                    session['logged_in'] = True
+                    flash('You are logged in as {}, Welcome!'.format(response['Item']['Name']))
+                    return redirect(url_for('login'))
+            else:
+                flash('There is no such user! Please Sign Up First!')
+                redirect(url_for('login'))
     return render_template('login.html', error=error)
 
 
@@ -34,7 +47,13 @@ def logout():
 def sign_up():
     error = None
     if request.method == 'POST':
-        return redirect(url_for('login'))
+        response = dynamodb.register_new_user(request.form['email'], request.form['name'], request.form['password'])
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return redirect(url_for('login'))
+        return {
+            'msg': 'Some error occcured',
+            'response': response
+        }
     return render_template('sign_up.html', error=error)
 
 
---
+++ controller.py
@@ -0,0 +1,65 @@
+from boto3 import resource
+import config
+
+AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+REGION_NAME = config.REGION_NAME
+
+resource = resource(
+    'dynamodb',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
+
+
+def create_table_user():
+    table = resource.create_table(
+        TableName='Library-System-UserTable',  # Name of the table
+        KeySchema=[
+            {
+                'AttributeName': 'Email',
+                'KeyType': 'HASH'  # RANGE = sort key, HASH = partition key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'Email',  # Name of the attribute
+                'AttributeType': 'S'  # N = Number (B= Binary, S = String)
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    return table
+
+
+UserTable = resource.Table('Library-System-UserTable')
+
+
+def register_new_user(email, name, password):
+    response = UserTable.put_item(
+        Item={
+            'Email': email,
+            'Name': name,
+            'Password': password,
+        }
+    )
+    return response
+
+
+def read_user_information(email):
+    response = UserTable.get_item(
+        Key={
+            'Email': email
+        },
+        AttributesToGet=[
+            'Email',
+            'Password',
+            'Name'
+        ]
+    )
+    return response
+
