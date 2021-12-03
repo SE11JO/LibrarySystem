381e9770818a1f1041dbde3cad9920b936c9e427
HoKwonLee <53327933+HoKwonLee@users.noreply.github.com>
2021-12-03 09:45:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import controller
+import json
 
-import boto3
 
 app = Flask(__name__)
 
-dynamodb_client = boto3.client('dynamodb')
+@app.route('/search', methods = ['GET', 'POST'])
+def home():
 
-table_name = 'TEST'
-
-@app.route("/", methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST' :
-        title = request.form['title']
-        
-        result = read_from_Table(title)
-        
     return render_template('search.html')
 
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        Title = request.form['title']
+        response = controller.search_library_book(Title)
+        json_string = json.dumps(response, ensure_ascii=False)
+        json_object = json.loads(json_string)
 
-def read_from_Table(id):
-    Table = dynamodb_client.Table('TEST')
-    response = Table.get_item(
-        Key={
-            'id': id
-        },
-        AttributesToGet=[
-            'name'
-        ]
-    )
-    return response
+        return render_template('result.html', data=json_object)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
--- controller.py
+++ controller.py
@@ -1,16 +1,28 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
+import config
 
-table_name = 'TEST'
+
+AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+REGION_NAME = config.REGION_NAME
+
+resource = boto3.resource(
+    'dynamodb',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
+
+table = resource.Table('Library')
 
 
-def read_from_Table(id, client):
-    Table = client.Table(table_name)
-    response = Table.get_item(
-        Key={
-            'id': id
-        },
-        AttributesToGet=[
-            'name'
-        ]
+def search_library_book (book_title) :
+    
+    response = table.scan(
+        FilterExpression=Attr('title').contains(book_title)
     )
-    return response
+
+    items = response['Items']
+
+    return items
