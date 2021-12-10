1d4c042617c0f0e8ec1ad0729ba41ab664e1bce8
HoKwonLee <53327933+HoKwonLee@users.noreply.github.com>
2021-12-10 12:56:56 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -9,15 +9,28 @@ app = Flask(__name__)
 def search():
     data = None
     if request.method == 'POST':
-        Title = request.form['title']
-        if Title != '' :
-            response = controller.search_library_book(Title)
-            dumps = json.dumps(response, ensure_ascii=False)
-            data = json.loads(dumps)
+        if request.form.get('title') != None :
+            Title = request.form['title']
+            
+            if Title != '' :
+                response = controller.search_library_book(Title)
+                dumps = json.dumps(response, ensure_ascii=False)
+                data = json.loads(dumps)
 
-        return render_template('search.html', data=data)
+                return render_template('search.html', data=data)
+
+        elif request.form.get('check') != None :
+            is_checked = request.form.getlist('check')
+            ### id값 데이터베이스로 넘기고 수정하는것만 하면됨.
 
     return render_template('search.html', data=data)    
+
+
+@app.route('/search/take', methods = ['GET','POST'])
+def take():
+    if request.method == 'POST':
+        is_checked = request.form.get('check')
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
