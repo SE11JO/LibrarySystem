74519fe3c6bd0fab030cf5ea04ab1d3b9a4f7747
shinmin9812 <shinmin9812@naver.com>
2021-12-17 16:07:01 +0900

    1217

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, render_template, request
 from boto3.dynamodb.conditions import Key, Attr
 import method as met
@@ -15,19 +14,20 @@ app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+def main():
+    return render_template('main.html')
 
 @app.route('/manage')
 def manage():
-    return render_template('manage.html')
+    response = met.select_all()
+    json_string = json.dumps(response, ensure_ascii=False)
+    json_object = json.loads(json_string)
 
+    return render_template('manage.html', object=json_object)
 
-@app.route('/create_book')
-def create_book():
-    return render_template('create_book.html')
-
+@app.route('/manage/create')
+def create():
+    return render_template('create.html')
 
 @app.route('/insert_db', methods=['POST'])
 def insert_db():
@@ -36,19 +36,22 @@ def insert_db():
     author = request.form['author']
     publish = request.form['publish']
 
-    suc = met.put_table(id, title, author, publish)
+    met.put_table(id, title, author, publish)
 
     return render_template('insert_db.html')
 
+@app.route('/manage/update', methods=["POST", "GET"])
+def update():
+    id = request.form.get['c_id']
+    print(id)
+    if id==None:
+        return render_template('manage.html')
 
-@app.route('/list_book')
-def list_book():
-    response = met.select_all()
+    response = met.get_book(id)
     json_string = json.dumps(response, ensure_ascii=False)
     json_object = json.loads(json_string)
 
-    return render_template('/list_book.html', object=json_object)
-
+    return render_template('update.html', object=json_object)
 
 @app.route('/update_db')
 def update_db():
@@ -58,11 +61,19 @@ def update_db():
     publish = request.form['publish']
     rental = request.form['rental']
     ren_date = request.form['ren_date']
+    ren_name = request.form['ren_name']
 
-    suc = met.update_table(id, title, author, publish, rental, ren_date)
+    met.update_table(id, title, author, publish, rental, ren_date, ren_name)
 
     return render_template('update_db.html')
 
+@app.route('/manage/delete', methods=["GET"])
+def delete_db():
+    id = request.form['c_title']
+    print(id)
+    met.delete_table(id)
+
+    return render_template('delete_db.html')
 
 if __name__ == '__main__':
     app.run()
--- method.py
+++ method.py
@@ -2,9 +2,11 @@ from pprint import pprint
 import boto3
 import json
 from botocore.exceptions import ClientError
+from flask.templating import render_template
 from flask.wrappers import Response
 from werkzeug.wrappers import response
 from flask import jsonify
+from boto3.dynamodb.conditions import Key, Attr
 
 
 def put_table(id, title, author, publish, dynamodb=None):
@@ -59,7 +61,7 @@ def select_all(dynamodb=None):
     return data
 
 
-def update_table(id, title, author, publish, rental, ren_date, dynamodb=None):
+def update_table(id, title, author, publish, rental, ren_date, ren_name, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource(
             'dynamodb',
@@ -76,7 +78,51 @@ def update_table(id, title, author, publish, rental, ren_date, dynamodb=None):
             'author': author,
             'publish': publish,
             'rental': rental,
-            'ren_date': ren_date
+            'ren_date': ren_date,
+            'ren_name': ren_name
         }
     )
     return response
+
+
+def get_book(id, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource(
+            'dynamodb',
+            aws_access_key_id='AKIARKVEYCRB5V6OTNXL',
+            aws_secret_access_key='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',
+            region_name='ap-northeast-2')
+
+    table = dynamodb.Table('library')
+
+    response = table.scan(
+        FilterExpression=Attr('id').contains(id)
+    )
+
+    items = response['Items']
+    return items
+
+
+def delete_table(id, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource(
+            'dynamodb',
+            aws_access_key_id='AKIARKVEYCRB5V6OTNXL',
+            aws_secret_access_key='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',
+            region_name='ap-northeast-2')
+
+    table = dynamodb.Table('library')
+
+    try:
+        response=table.delete_item(
+            Key={
+                'id':id
+            }
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(e.response['Error']['Message'])
+        else:
+            raise
+    else:
+        return response
