ffd71cd9b005a05b4cf7a417ef632b26537cf17c
김동현 <41746120+zoown13@users.noreply.github.com>
2021-12-31 19:50:54 +0900

    AWS ACCESS KEY REMOVED
    보안을 위해 ACCESS KEY 삭제

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # root access key
-AWS_ACCESS_KEY_ID = 'AKIA3HMQD2NB4ZQXCIEU'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = '' # removed for security reason 
+AWS_SECRET_ACCESS_KEY = '' # removed for security reason 
 REGION_NAME = 'ap-northeast-2'
 USER_TABLE_NAME = 'Library-System-UserTable'
 BOOK_TABLE_NAME = 'library'
