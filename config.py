2cd32a7755ac52fbff6a3b7649824fc4137ef4d5
HoKwonLee <53327933+HoKwonLee@users.noreply.github.com>
2022-01-04 11:49:14 +0900

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# root access key
-AWS_ACCESS_KEY_ID = 'AKIA23QNNSOOAROXZCU7'
-AWS_SECRET_ACCESS_KEY = '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'
-REGION_NAME = 'ap-northeast-2'
-BOOK_TABLE_NAME = 'Library'
